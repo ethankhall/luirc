8343e85b39cb0ee2bb9ea38311bb9ffca67f38af
ethankhall <ethankhall@24a8315c-e80f-11de-83d8-6d5adf01d787>
2009-12-14 07:00:18 +0000

    Added Licensing things to the files along with a README.

--- trunk/kernel_driver/luirc.c
+++ trunk/kernel_driver/luirc.c
@@ -10,6 +10,21 @@
 #include <linux/fs.h>
 #include <asm/uaccess.h>  
 
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*  
   *	Prototypes - this would normally go in a .h file
   */
@@ -37,7 +52,6 @@ static char *msg_Ptr;
 
 static struct input_dev *luirc_dev;	
 static struct file_operations fops = {
-  .owner = THIS_MODULE,
   //.read = device_read, 
   .write = device_write,
   .open = device_open,

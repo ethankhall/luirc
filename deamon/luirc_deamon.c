52d671d1d6e889a1f7b4d6e529a456ded64659ba
ethankhall <ethankhall@24a8315c-e80f-11de-83d8-6d5adf01d787>
2009-12-15 05:38:39 +0000

    Created Makefile for ease of compiling. Renamed luirc.c to luirc_deamon.c so it becomes easier to distinguish between files. created the preferences.c/h files. This will be for getting preferences from a file. Created luirc_deamon.h so that I can have an H file for it.

--- deamon/luirc_deamon.c
+++ deamon/luirc_deamon.c
@@ -1,27 +1,17 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-//#include <termios.h>
-#include <fcntl.h>
-#include <syslog.h>
-#include <signal.h>
-#include <getopt.h>
-#include <unistd.h>  /* _exit, fork */
-#include <errno.h>   /* errno */
-
+#include "luirc_deamon.h"
 /*
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*	This is the pcaart of the code that lets you define what keys are what
@@ -32,15 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *	block later will tell you how they are used.
  */
 
-#define VERSION_NUMBER "0.1"
-#define UPARROW "61A042BD"
-#define DOWNARROW "61A0C23D"
-#define LEFTARROW "61A06897"
-#define RIGHTARROW "61A0A857"
-#define ENTER "61A018E7"
-#define EXIT "61A0D827"
-#define CHUP "61A050AF"
-#define CHDOWN "61A0D02F"
+
+/*
+ *	The function prototypes
+ */
 
 void cleanup(int signal);
 
@@ -49,32 +34,30 @@ void cleanup(int signal);
  *	These will be things like debug enables and adding different modes.
  */
 
-static int verbose_flag;
-static int deamon_flag = 1;
-char device[30] = "/dev/ttyUSB0"; //The device that we will be using for the program.
+
 FILE * filetoopen;	//File pointer to the serial (usb serial?) device
 FILE * deviceDriver;
+FILE * preferences;
+
+static struct option long_options[] =
+{
+	/* These options set a flag. */
+	{"verbose", no_argument,       &verbose_flag, 1},
+	{"debug", 	no_argument, 	&verbose_flag, 1},
+	{"d",  			no_argument,  &verbose_flag, 1},
+	{"port",  	required_argument, 0, 'a'},
+	{"p",		  	required_argument, 0, 'a'},
+	{"no-deamon", no_argument, &deamon_flag, 0},
+	{"version", no_argument, 0, 'b'},
+	{"v", 			no_argument, 0, 'b'},
+	{"h", 			no_argument, 0, 'h'},
+	{"help", 			no_argument, 0, 'h'},
+	{0, 0, 0, 0}
+};
+	
 /*
  *	END OF THE GLOBAL VARIABLES
  */
-
-//Handel when the program has to exit....
-void  cleanup(int signal){
-	if (verbose_flag) printf("Exiting program...\nGoodbye!\n");
-	printf("\n");
-	syslog (LOG_NOTICE, "luirc deamon exiting nicely...");
-	closelog ();
-	if (filetoopen)
-	{
-		fclose(filetoopen);
-	}
-	if ( deviceDriver ){
-		fclose(deviceDriver);
-	}
-	return;
-}
-
-
 
 int main(int argc, char* argv[]){
 	int i = 0;
@@ -86,24 +69,11 @@ int main(int argc, char* argv[]){
 	//	Signal Stuff (closing files on program close)
 	//
 	
+	preferences = fopen("luirc.conf", "r");
+	
 	signal(SIGINT,cleanup); 
 	signal(SIGTERM,cleanup); 
 	
-	static struct option long_options[] =
-	{
-		/* These options set a flag. */
-		{"verbose", no_argument,       &verbose_flag, 1},
-		{"debug", 	no_argument, 	&verbose_flag, 1},
-		{"d",  			no_argument,  &verbose_flag, 1},
-		{"port",  	required_argument, 0, 'a'},
-		{"p",		  	required_argument, 0, 'a'},
-		{"no-deamon", no_argument, &deamon_flag, 0},
-		{"version", no_argument, 0, 'b'},
-		{"v", 			no_argument, 0, 'b'},
-		{"h", 			no_argument, 0, 'h'},
-		{"help", 			no_argument, 0, 'h'},
-		{0, 0, 0, 0}
-	};
 	while (1){
 	
 		option_index = 0;
@@ -145,7 +115,20 @@ int main(int argc, char* argv[]){
 		}
 	}
 	
+	/*
+	 * 		Show the of the preferences
+	 */
 	
+	if (preferences) {
+		return -1;
+	}
+	
+	if(verbose_flag){
+		printf("Here are the preferences that you have loaded:\n");
+		for(i = 0; i < 8; i++){
+			printf("prefArray[%s] = \"%s\"\n", prefIndex[i], prefArray[i]);
+		}
+	}
 	
 	
 	
@@ -208,42 +191,42 @@ int main(int argc, char* argv[]){
 			raise(SIGTERM);
 		}
 		output[8] = '\0';
-		if(strstr(output, UPARROW)){ 
+		if(strstr(output, prefArray[UPARROW])){ 
 			if(verbose_flag) printf("Uparrow\n");
 			//Send the up arrow key
 			fprintf(deviceDriver, "%c", (char)103);
 			
-		} else if (strstr(output, DOWNARROW)){
+		} else if (strstr(output, prefArray[DOWNARROW])){
 			if(verbose_flag) printf("Down\n");
 			//Send the down arrow key
 			fprintf(deviceDriver, "%c", (char)108);
 			
-		} else if (strstr(output, LEFTARROW)){
+		} else if (strstr(output, prefArray[LEFTARROW])){
 			if(verbose_flag) printf("Left\n");
 			//Send the left arrow key
 			fprintf(deviceDriver, "%c", (char)105);
 			
-		} else if (strstr(output, RIGHTARROW )){
+		} else if (strstr(output, prefArray[RIGHTARROW])){
 			if(verbose_flag) printf("Right\n");
 			//Send the right arrow key
 			fprintf(deviceDriver, "%c", (char)106);
 			
-		} else if (strstr(output, ENTER)){
+		} else if (strstr(output, prefArray[ENTER])){
 			if(verbose_flag) printf("Enter\n");
 			//Send the enter key
 			fprintf(deviceDriver, "%c", (char)28);
 			
-		} else if (strstr(output, EXIT)){
+		} else if (strstr(output, prefArray[EXIT])){
 			if(verbose_flag) printf("Exit\n");
 			//Send the escape key
 			fprintf(deviceDriver, "%c", (char)1);
 			
-		} else if (strstr(output, CHUP)){
+		} else if (strstr(output, prefArray[CHUP])){
 			if(verbose_flag) printf("CH UP\n");
 			//Send the pageup key
 			fprintf(deviceDriver, "%c", (char)104);
 			
-		} else if (strstr(output, CHDOWN)) {
+		} else if (strstr(output, prefArray[CHDOWN])) {
 			if(verbose_flag) printf("CH DOWN\n");
 			fprintf(deviceDriver, "%c", (char)109);
 		} else {
@@ -254,6 +237,20 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
+//Handel when the program has to exit....
+void  cleanup(int signal){
+	if (verbose_flag) printf("Exiting program...\nGoodbye!\n");
+	syslog (LOG_NOTICE, "luirc deamon exiting nicely...");
+	closelog ();
+	if (filetoopen)
+	{
+		fclose(filetoopen);
+	}
+	if ( deviceDriver ){
+		fclose(deviceDriver);
+	}
+	return;
+}
 
 //End of Program
 

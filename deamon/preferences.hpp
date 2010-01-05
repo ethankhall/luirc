67593432306fe94c2c6b67db4bf3293c8432e3f0
ethankhall <ethankhall@24a8315c-e80f-11de-83d8-6d5adf01d787>
2010-01-05 23:22:41 +0000

    Forgot to include these files in the inital commit. Not tested just getting it one to the SVN server.  Testing to come later.. Keep looking.

---
+++ deamon/preferences.hpp
@@ -0,0 +1,33 @@
+#ifndef _preferences_hpp_
+#define _preferences_hpp_
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+#define VERSION_NUMBER "0.2"
+
+class RemotePreferences {
+	std::map<std::string,std::string> mapData;
+	std::map<std::string,std::string>::iterator mapData_itter;
+	public:
+		void insertToHash(std::string key, std::string value);
+		void displayMap(void);
+		std::string searchHash(std::string key);
+};
+
+class ButtonPreferences {
+	std::map<std::string,char> mapData;
+	std::map<std::string,char>::iterator mapData_itter;
+	public:
+		void insertToHash(std::string key, char value);
+		void displayMap(void);
+		char searchHash(std::string key);
+};
+
+void getConfigOptions(RemotePreferences & prefs, std::string fileName);
+
+#endif
